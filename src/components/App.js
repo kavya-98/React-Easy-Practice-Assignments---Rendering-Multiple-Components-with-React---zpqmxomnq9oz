8ff824e05612e5200ab4c06ec8e31d9cfb07e247
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-04-26 20:10:59 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const [name, setname] = useState("abc");
+  const [state, setstate] = useState("xyz");
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <div data-ns-test="project-name">{name}</div>
+      <div data-ns-test="project-description">{state}</div>
+    </div>
+  );
+};
 
 export default App;
